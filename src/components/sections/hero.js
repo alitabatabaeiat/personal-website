1dfdfc60f5caf4047c878b0ac0ee263b056c8eb6
Ali Tabatabaei <a.tabatabaei97@gmail.com>
2022-02-24 04:17:53 -0500

    information updated

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -3,7 +3,7 @@ import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import styled from 'styled-components';
 import { navDelay, loaderDelay } from '@utils';
 import { usePrefersReducedMotion } from '@hooks';
-// import { email } from '@config';
+import { basic } from '@config';
 
 const StyledHeroSection = styled.section`
   ${({ theme }) => theme.mixins.flexCenter};
@@ -59,28 +59,25 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
+  const two = <h2 className="big-heading">{basic.fullName}</h2>;
   const three = <h3 className="big-heading">I build things for the web.</h3>;
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
-        at{' '}
-        <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-          Upstatement
-        </a>
-        .
+        I’m a software developer who love build and develop web applications, work with others, and
+        solve different problems. Currently I'm in Toronto, and enjoying winter.{' '}
+        <span role="img" aria-label="snow">
+          ⛄️❄️
+        </span>
       </p>
     </>
   );
   const five = (
-    <a
-      className="email-link"
-      href="https://www.newline.co/courses/build-a-spotify-connected-app"
-      target="_blank"
-      rel="noreferrer">
-      Check out my course!
+    <a className="email-link" href={`mailto:${basic.email}`}>
+      Get In Touch{' '}
+      <span role="img" aria-label="mail">
+        📫
+      </span>
     </a>
   );
 
