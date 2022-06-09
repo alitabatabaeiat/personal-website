91dc2840c0ca4baec016842e511b7dbe1882ec6d
Ali Tabatabaei <a.tabatabaei97@gmail.com>
2022-06-09 17:14:31 -0400

    Google analytics configured

--- gatsby-config.js
+++ gatsby-config.js
@@ -150,9 +150,17 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        trackingId: 'UA-45666519-2',
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          'G-02DBBGDLMJ', // Google Analytics / GA
+        ],
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+        },
       },
     },
   ],
